1187bb70800f378919dab45fbefbead9b66bc57b
guramrit.dhillon <guramrit@msn.com>
2026-03-19 23:47:17 -0400

    feat(metadata): add FormViewer entry point and CSS

---
+++ packages/dataverse-metadata/views/formViewer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "shared-views";
+import "shared-views/panel.css";
+import "shared-views/fullpage.css";
+import "./styles/viewDesigner.css";
+import "./styles/formViewer.css";
+import { FormViewer } from "./components/FormViewer";
+
+const root = document.getElementById("root");
+if (root) {
+  createRoot(root).render(<ErrorBoundary><FormViewer /></ErrorBoundary>);
+}
